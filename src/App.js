6be6abebaff8e699e19168dada1a35fefb0efaa5
Anthony K <antonyk@users.noreply.github.com>
2020-03-25 22:38:42 -0400

    readme merge; components folder; header as component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+// core imports
 import React from 'react';
-import logo from './logo.svg';
+
+// style imports
 import './App.css';
+import {Button} from 'reactstrap'
+
+// import components
+import HeaderComponent from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderComponent />
     </div>
   );
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+const tmp = {}
+
+function Init() {
+  tmp['hello'] = 'hello'
+}
+
+
+function SignupForm() {
+
+  return (
+
+    <form>
+
+      Name
+      Email
+      Password
+      Terms (checkbox)
+      Submit Btn
+
+    </form>
+
+  )
+
+}
+
+export default Signup;
